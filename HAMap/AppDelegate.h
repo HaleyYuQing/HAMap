497cbe6889cffcd033dc2dbd7cb3a93e648742d4
Haley <haley@justek.us>
2018-11-20 11:35:47 +0800

    Add HAMap

---
+++ HAMap/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HAMap
+//
+//  Created by yuqing huang on 19/11/2018.
+//  Copyright © 2018 Justek. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HAMap/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HAMap
+//
+//  Created by yuqing huang on 19/11/2018.
+//  Copyright © 2018 Justek. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
